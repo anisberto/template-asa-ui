7d374fdda1b159d3814d22e3525f82d7670d0cca
anisberto reis <anisberto.nascimento@contrato.g4f.com.br>
2022-11-11 18:41:40 -0300

    configuração de stilos e scripts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,11 @@ import { AEmpresaComponent } from './paginas/a-empresa/a-empresa.component';
 import { CatalogoDeProdutosComponent } from './paginas/catalogo-de-produtos/catalogo-de-produtos.component';
 import { TrabalheConoscoComponent } from './paginas/trabalhe-conosco/trabalhe-conosco.component';
 import { ContatoComponent } from './paginas/contato/contato.component';
+import { CarouselComponent } from './components/carousel/carousel.component';
+import { FormularioComponent } from './components/formulario/formulario.component';
+import { TabelaComponent } from './components/tabela/tabela.component';
+import { GraficoComponent } from './components/grafico/grafico.component';
+import { ModalComponent } from './components/modal/modal.component';
 
 @NgModule({
   declarations: [
@@ -20,7 +25,12 @@ import { ContatoComponent } from './paginas/contato/contato.component';
     AEmpresaComponent,
     CatalogoDeProdutosComponent,
     TrabalheConoscoComponent,
-    ContatoComponent
+    ContatoComponent,
+    CarouselComponent,
+    FormularioComponent,
+    TabelaComponent,
+    GraficoComponent,
+    ModalComponent
   ],
   imports: [
     BrowserModule,
